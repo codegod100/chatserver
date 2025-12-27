39efa4d29b5b8c1f14207a0cfc1c23abcc5f100e
nandi <78769380+codegod100@users.noreply.github.com>
2025-12-27 01:52:55 +0000

    Add .gitignore for build artifacts and caches

--- .zig-cache/c/91e10ca703ce1200c44ed0f388a3a61e/options.zig
+++
@@ -1,8 +0,0 @@
-pub const enable_tracy: bool = false;
-pub const trace_eval: bool = true;
-pub const trace_refcount: bool = false;
-pub const compiler_version: []const u8 = "debug-40abd1ab";
-pub const enable_tracy_callstack: bool = false;
-pub const enable_tracy_allocation: bool = false;
-pub const tracy_callstack_depth: u32 = 10;
-pub const target_is_native: bool = false;
